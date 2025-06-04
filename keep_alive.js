5ae24ab251dc3fb841d7fa437e2a05294bd0b95c
youneskirkoloft <yuneskirkoloft@gmail.com>
2025-06-04 15:21:43 +0330

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 8000;
+
 app.get('/', (req, res) => {
   res.send('Bot has arrived');
 });
 
-app.listen(8000, () => {
-  console.log('Server started');
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
